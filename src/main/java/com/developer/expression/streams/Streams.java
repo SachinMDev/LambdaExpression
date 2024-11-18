d8779eade1c2d61676788f5a29edfbd3b9721cab
SachinMishra <mishrasachin96@gmail.com>
2024-11-18 11:22:10 +0530

    first commit

---
+++ src/main/java/com/developer/expression/streams/Streams.java
@@ -0,0 +1,11 @@
+package com.developer.expression.streams;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Streams {
+	
+	public void firstExpression() {
+		System.out.println("This is my first lambda expression");
+	}
+}
